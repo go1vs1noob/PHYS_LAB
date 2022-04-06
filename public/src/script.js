81468c0845949f5dab67bd4e816d4c08404fb441
go1vs1noob <90688133+go1vs1noob@users.noreply.github.com>
2022-04-06 19:54:04 +0300

    нить

--- public/src/script.js
+++ public/src/script.js
@@ -1,7 +1,6 @@
 var Stopwatch = function (elem, options) {
   /* СЕКУНДОМЕР. ЗАПРАШИВАЕТ ДАТУ И ЧЕРЕЗ ДЕЛЬТУ ОБНОВЛЯЕТ СЧЕТЧИК */
   var timer = createTimer(),
-
     offset,
     clock,
     interval;
@@ -12,7 +11,7 @@ var Stopwatch = function (elem, options) {
 
   // append elements
   elem.appendChild(timer);
- /* elem.appendChild(startButton);
+  /* elem.appendChild(startButton);
   elem.appendChild(stopButton);
   elem.appendChild(resetButton);*/
 
@@ -94,33 +93,30 @@ let button_weight_1 = document.getElementById("button_weight_1");
 let button_weight_2 = document.getElementById("button_weight_2");
 let button_weight_3 = document.getElementById("button_weight_3"); // ИНИЦИАЛИЗАЦИЯ ОБЪЕКТОВ
 let button_weight_4 = document.getElementById("button_weight_4");
-let dummy_button = document.getElementById("dummy_button");
-
-
-
-
+let thread_left = document.getElementById("thread_left");
+let thread_right = document.getElementById("thread_right");
 
 let added_mass = 0; // ИНИЦИАЛИЗИРУЕМ МАССУ, ДОБАВЛЕННУЮ ГРУЗИКАМИ
-let wht1_flag = 0; // ИНИЦИАЛИЗИРУЕМ ФЛАГИ, ОЗНАЧАЮЩИЕ, ЧТО ГРУЗИК X БЫЛ ВКЛЮЧЕН 
+let wht1_flag = 0; // ИНИЦИАЛИЗИРУЕМ ФЛАГИ, ОЗНАЧАЮЩИЕ, ЧТО ГРУЗИК X БЫЛ ВКЛЮЧЕН
 let wht2_flag = 0;
 let wht3_flag = 0;
 let wht4_flag = 0;
 let flag_sum = 0;
 const cargo_height = document.getElementById("cargo_blue").offsetHeight; // ВЫСОТА ГРУЗИКА
 
-button_start.addEventListener("click", (e) => { 
-  if (flag_sum !=0){
+button_start.addEventListener("click", (e) => {
+  if (flag_sum != 0) {
     button_reset.disabled = true;
     button_start.disabled = true;
     turn_off_weight_buttons();
     move();
   }
-  
 });
-button_reset.addEventListener("click", (e) => {        // ДОБАВЛЯЕМ КНОПКИ В EventListener (обработка кнопок много раз)
-    reset();
-    turn_on_weight_buttons();
-    button_start.disabled = false;
+button_reset.addEventListener("click", (e) => {
+  // ДОБАВЛЯЕМ КНОПКИ В EventListener (обработка кнопок много раз)
+  reset();
+  turn_on_weight_buttons();
+  button_start.disabled = false;
 });
 button_weight_1.addEventListener("click", (e) => {
   wht1_init();
@@ -135,11 +131,7 @@ button_weight_4.addEventListener("click", (e) => {
   wht4_init();
 });
 
-
-
-
 function move() {
-
   let weight_1 = document.getElementById("weight_1"); // ИНИЦИАЛИЗИРУЕМ ГРУЗИКИ
   let weight_2 = document.getElementById("weight_2");
   let weight_3 = document.getElementById("weight_3");
@@ -149,6 +141,8 @@ function move() {
   let currentWeight_2 = weight_2.offsetTop;
   let currentWeight_3 = weight_3.offsetTop;
   let currentWeight_4 = weight_4.offsetTop;
+  let currentThreadLeft = thread_left.offsetHeight;
+  let currentThreadRight = thread_right.offsetHeight;
 
   let cargo_blue = document.getElementById("cargo_blue"); // ИНИЦИАЛИЗИРУЕМ ГРУЗИКИ С ПОМОЩЬЮ getElementById
   let cargo_red = document.getElementById("cargo_red");
@@ -159,20 +153,19 @@ function move() {
   const cargoToStop = GlobalCargoredPos; // Координаты для остановки правого грузика (изначальные координаты левого)
   let pixelsToMove = 0; // На сколько пикселей двигать грузики при каждом вызове функции
   let sensorPos = sensor.offsetTop; // Позиция сенсора
-  
+
   let acceleration =
     9.806 * 17 * ((81.1 + added_mass - 81.1) / (81.1 + 81.1 + added_mass)); // g = 9.8, 17px/cm, 81.1 = m;
-  
+
   let animateInterval = setInterval(animate, 10); // Каждые 10мс вызывается функция animate(), пока не прирвём с помощью clearInterval(animate). Значение 10 можно менять
   timer.start(); // Запускаем секундомер
-    
+
   function animate() {
     pixelsToMove = acceleration * timer.curr_time(); //равноуск. движ. t = 139 px/с^2 для m1 = 82.5, m2 =
     if (currentCargoblue >= cargoToStop) {
       //Если правый грузик достиг места остановки,
       clearInterval(animateInterval); // то прерываем animate() [движение]  TODO: ПОФИКСИТЬ ВРЕЗАНИЕ В СТОЙКУ. СКОРЕЕ ВСЕГО ПРОИСХОДИТ, ПОТОМУ ЧТО ДВИГАЕТСЯ БОЛЬШЕ ЧЕМ НА 1 ПИКСЕЛЬ ЗА ИТЕРАЦИЮ
-      button_reset.disabled = false;  // КОГДА ГРУЗИК ОСТАНАВЛИВАЕТСЯ ПОЗВОЛЯЕМ ПОЛЬЗОВАТЕЛЮ СДЕЛАТЬ СБРОС
-
+      button_reset.disabled = false; // КОГДА ГРУЗИК ОСТАНАВЛИВАЕТСЯ ПОЗВОЛЯЕМ ПОЛЬЗОВАТЕЛЮ СДЕЛАТЬ СБРОС
     } else {
       if (currentCargoblue >= sensorPos - cargo_height) {
         //Если правый грузик достиг линии сенсора фотодатчика
@@ -180,10 +173,17 @@ function move() {
       }
       currentCargored -= pixelsToMove; // В ином случае двигаем правый грузик вниз, а левый вверх на {pixelsToMove} пикселей (значение изменяемое)
       currentCargoblue += pixelsToMove;
+
       currentWeight_1 += pixelsToMove;
       currentWeight_2 += pixelsToMove; // ВМЕСТЕ С НИМИ ДВИГАЕМ И ДОБАВЛЕННЫЕ ГРУЗИКИ
       currentWeight_3 += pixelsToMove;
       currentWeight_4 += pixelsToMove;
+
+      currentThreadLeft -= pixelsToMove;
+      currentThreadRight += pixelsToMove;
+
+      thread_right.style.height = currentThreadRight + "px";
+      thread_left.style.height = currentThreadLeft + "px";
       cargo_red.style.top = currentCargored + "px"; // Переприсваиваем новое положение в css с помощью style.top
       cargo_blue.style.top = currentCargoblue + "px";
       weight_1.style.top = currentWeight_1 + "px"; // ОБНОВЛЯЕМ ИХ ПОЗИЦИИ В CSS
@@ -195,7 +195,6 @@ function move() {
 }
 
 function reset() {
-
   // Возвращаем таймер и грузики в изначальное положение
   cargo_blue.style.top = GlobalCargobluePos + "px"; // Возвращаем на изначальные позиции
   cargo_red.style.top = GlobalCargoredPos + "px";
@@ -203,13 +202,15 @@ function reset() {
   weight_2.style.visibility = "hidden";
   weight_3.style.visibility = "hidden";
   weight_4.style.visibility = "hidden";
+  thread_left.style.height = 600 + "px";
+  thread_right.style.height = 60 + "px";
   wht1_flag = 0;
   wht2_flag = 0;
   wht3_flag = 0; // ФЛАГИ, ЧТО ДОБАВЛЕН X ГРУЗИК
   wht4_flag = 0;
   added_mass = 0;
   flag_sum = 0;
-  timer.stop();  // Останавливаем таймер
+  timer.stop(); // Останавливаем таймер
   timer.reset(); // Обнуляем таймер
 }
 
@@ -221,6 +222,7 @@ function wht1_init() {
     weight_1.style.left =
       document.getElementById("cargo_blue").offsetLeft + "px"; // ЛЕВАЯ ПОЗИЦИЯ (ТАКАЯ ЖЕ, КАК У СИНЕГО)
     if (flag_sum == 1) {
+      turn_off_weight_buttons();
       weight_1.style.top =
         document.getElementById("cargo_blue").offsetTop - 40 + "px"; // TOP ПОЗИЦИЯ (КАК У СИНЕГО, НО МЕНЬШЕ НА 40 ЕСЛИ 1 ГРУЗИК УЖЕ ДОБАВЛЕН)
     } else {
@@ -240,6 +242,7 @@ function wht2_init() {
     weight_2.style.left =
       document.getElementById("cargo_blue").offsetLeft + "px";
     if (flag_sum == 1) {
+      turn_off_weight_buttons();
       weight_2.style.top =
         document.getElementById("cargo_blue").offsetTop - 40 + "px";
     } else {
@@ -259,6 +262,7 @@ function wht3_init() {
     weight_3.style.left =
       document.getElementById("cargo_blue").offsetLeft + "px";
     if (flag_sum == 1) {
+      turn_off_weight_buttons();
       weight_3.style.top =
         document.getElementById("cargo_blue").offsetTop - 40 + "px";
     } else {
@@ -277,6 +281,7 @@ function wht4_init() {
     weight_4.style.left =
       document.getElementById("cargo_blue").offsetLeft + "px";
     if (flag_sum == 1) {
+      turn_off_weight_buttons();
       weight_4.style.top =
         document.getElementById("cargo_blue").offsetTop - 40 + "px";
     } else {
@@ -292,14 +297,14 @@ function wht4_init() {
 
 function turn_off_weight_buttons() {
   button_weight_1.disabled = true;
-  button_weight_2.disabled = true;      // ОТКЛЮЧЕНИЕ КНОПОК ГРУЗОВ
+  button_weight_2.disabled = true; // ОТКЛЮЧЕНИЕ КНОПОК ГРУЗОВ
   button_weight_3.disabled = true;
   button_weight_4.disabled = true;
 }
 
 function turn_on_weight_buttons() {
   button_weight_1.disabled = false;
-  button_weight_2.disabled = false;    // ВКЛЮЧЕНИЕ КНОПОК ГРУЗОВ
+  button_weight_2.disabled = false; // ВКЛЮЧЕНИЕ КНОПОК ГРУЗОВ
   button_weight_3.disabled = false;
   button_weight_4.disabled = false;
 }
